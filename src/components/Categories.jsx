6e52c328d9d52073af09fb6263ac3e31d28b15cd
Kenan <kenan.haciyev.123@bk.ru>
2023-07-03 10:47:01 +0400

    feat(props): send information for pizza through props

---
+++ src/components/Categories.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+function Categories() {
+    const [activeIndx, setActiveIndex] = useState(0)
+    const categories = ["Все", "Мясные", "Вегетерианская", "Гриль", "Острые", "Закрытые"]
+
+    function onClickCategory(i) {
+        setActiveIndex(i)
+    }
+
+    return (
+        <div className="categories">
+            <ul>
+                {
+                    categories.map((value, i) => {
+                        return (
+                            <li onClick={() => onClickCategory(i)} className={activeIndx === i ? "active" : null}>{value}</li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default Categories
